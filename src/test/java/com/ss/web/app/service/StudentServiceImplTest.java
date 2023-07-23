7aed4f7efa490b8f6a8478b4a5eac559519df1ce
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-07-23 19:12:59 -0400

    test for creation edit and delete added

--- src/test/java/com/ss/web/app/service/StudentServiceImplTest.java
+++ src/test/java/com/ss/web/app/service/StudentServiceImplTest.java
@@ -3,6 +3,9 @@ package com.ss.web.app.service;
 import com.ss.web.app.model.Student;
 import com.ss.web.app.repository.StudentRepo;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,10 +24,44 @@ class StudentServiceImplTest {
     assertNotNull(studentExpected);
     assertEquals(student, studentExpected);
   }
+    @Test
+    public void findAll() {
+        //Arrange
+        StudentRepo repo = mock(StudentRepo.class);
+        StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+        Student student1 = new Student(1L, "Sho", "Test");
+        Student student2 = new Student(2L, "Auto", "Test");
+        List<Student> students = Arrays.asList(student1, student2);
+        when(repo.findAll()).thenReturn(students);
+        List<Student> studentsExpected = studentServiceImpl.findAll();
 
-//  @Test
-//  public void findAllTest(){
-//    StudentService studentService = new StudentService();
-//    studentService
-//  }
+        assertNotNull(studentsExpected);
+        assertEquals(2, studentsExpected.size());
+        assertEquals(students, studentsExpected);
+    }
+
+    @Test
+    public void edit() {
+        StudentRepo repo = mock(StudentRepo.class);
+        StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+        Student student = new Student(1L, "Sho", "Test");
+        student.setName("Edited");
+        when(repo.edit(student)).thenReturn(student);
+        Student studentExpected = studentServiceImpl.edit(student);
+
+        assertNotNull(studentExpected);
+        assertEquals("Edited", studentExpected.getName());
+        assertEquals(student, studentExpected);
+    }
+
+    @Test
+    public void delete() {
+        StudentRepo repo = mock(StudentRepo.class);
+        StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+        Student student = new Student(1L, "Sho", "Test");
+        when(repo.delete(student)).thenReturn(true);
+        boolean isDeleted = studentServiceImpl.delete(student);
+
+        assertTrue(isDeleted);
+    }
 }
